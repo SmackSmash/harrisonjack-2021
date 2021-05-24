f5543d5dfbc95a89b8526b0bf0a9d5817b5af46a
Dan Moffat <danmoffat@gmail.com>
2021-05-24 21:34:15 +0100

    Restructued basic installation and added node-sass

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.scss';
+
+const App = () => {
+  return (
+    <div className="app">
+      <h1>App Works!</h1>
+    </div>
+  );
+};
+
+export default App;
