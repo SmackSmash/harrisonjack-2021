ee2dbca2cdda80267301962077780c96053304b7
SmackSmash <danmoffat@gmail.com>
2021-05-26 00:39:41 +0100

    Little reminder on mapDispatchToProps

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import { connect } from 'react-redux';
-// import { increment, decrement, reset } from '../../actions';
+import { increment, decrement, reset } from '../../actions';
 import './Counter.scss';
 
-const Counter = ({ count }) => {
+const Counter = ({ count, increment, decrement, reset }) => {
   return (
     <div className="counter">
       <h2 className="count">The count is {count}</h2>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
+      <button onClick={reset}>Reset</button>
     </div>
   );
 };
 
 const mapStateToProps = ({ count }) => ({ count });
 
-export default connect(mapStateToProps)(Counter);
+export default connect(mapStateToProps, { increment, decrement, reset })(Counter);
