4cdc9c6cf7edfcd553e14584241d5df7d94b9114
Dan Moffat <danmoffat@gmail.com>
2021-06-01 22:14:48 +0100

    Added Swiper gallery to home page

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,7 +1,27 @@
 import React from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/swiper.scss';
 
 const Home = () => {
-  return <h2>Home page works</h2>;
+  return (
+    <Swiper>
+      <SwiperSlide>
+        <img src="http://lorempixel.com/1600/900/" />
+      </SwiperSlide>
+      <SwiperSlide>
+        <img src="http://lorempixel.com/1600/900/" />
+      </SwiperSlide>
+      <SwiperSlide>
+        <img src="http://lorempixel.com/1600/900/" />
+      </SwiperSlide>
+      <SwiperSlide>
+        <img src="http://lorempixel.com/1600/900/" />
+      </SwiperSlide>
+      <SwiperSlide>
+        <img src="http://lorempixel.com/1600/900/" />
+      </SwiperSlide>
+    </Swiper>
+  );
 };
 
 export default Home;
